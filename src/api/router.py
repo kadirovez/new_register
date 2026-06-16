001c908e767dda50353add3b9f566c57e32c872a
faroosha <kadirovezfarhad@gmail.com>
2026-06-16 09:25:55 +0400

    small update

--- src/api/router.py
+++ src/api/router.py
@@ -4,8 +4,12 @@ from fastapi import APIRouter
 from src.api.auth.login import router as login_router
 from src.api.auth.registration import router as registration_router
 from src.api.auth.forgot_password import router as forgot_password_router
+from src.api.metrics import router as metrics_router
+from src.api.users import router as users_router
 
 api_router = APIRouter()
 api_router.include_router(registration_router)
 api_router.include_router(login_router)
 api_router.include_router(forgot_password_router)
+api_router.include_router(users_router)
+api_router.include_router(metrics_router)
